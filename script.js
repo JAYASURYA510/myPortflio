d15f75460d00d1c8c50e57e6927da269190cf979
JAYASURYA510 <suryatheforgiver@gmail.com>
2023-11-04 20:07:04 +0530

    Resume edited

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const downloadButton = document.getElementById('downloadButton');
 
 downloadButton.addEventListener('click', function() {
-  const pdfUrl = 'my resume.docx.pdf';
+  const pdfUrl = 'my Resume12.pdf';
   const link = document.createElement('a');
   link.href = pdfUrl;
   link.download = 'resume.pdf';
@@ -9,4 +9,17 @@ downloadButton.addEventListener('click', function() {
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
+});
+
+const viewButton = document.getElementById('viewButton');
+const resumeContainer = document.getElementById('resumeContainer');
+
+viewButton.addEventListener('click', function() {
+  const pdfUrl = 'my Resume12.pdf';
+  const embedElement = document.createElement('embed');
+  embedElement.src = pdfUrl;
+  embedElement.width = '100%';
+  embedElement.height = '600px';
+  resumeContainer.innerHTML = '';
+  resumeContainer.appendChild(embedElement);
 });
