02a88880f6e3124f1b7b170b45f9f669fcb74025
JAYASURYA510 <suryatheforgiver@gmail.com>
2023-12-21 18:15:13 +0530

    Resume updet

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const downloadButton = document.getElementById('downloadButton');
 
 downloadButton.addEventListener('click', function() {
-  const pdfUrl = 'my Resume12.pdf';
+  const pdfUrl = 'my Resume``.pdf';
   const link = document.createElement('a');
   link.href = pdfUrl;
   link.download = 'resume.pdf';
@@ -15,11 +15,27 @@ const viewButton = document.getElementById('viewButton');
 const resumeContainer = document.getElementById('resumeContainer');
 
 viewButton.addEventListener('click', function() {
-  const pdfUrl = 'my Resume12.pdf';
+  const pdfUrl = 'my Resume``.pdf';
   const embedElement = document.createElement('embed');
   embedElement.src = pdfUrl;
   embedElement.width = '100%';
   embedElement.height = '600px';
   resumeContainer.innerHTML = '';
   resumeContainer.appendChild(embedElement);
+});
+const feedbackForm = document.getElementById('feedbackForm');
+
+feedbackForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const rating = document.getElementById('rating').value;
+  const feedback = document.getElementById('feedback').value;
+  
+  // Here, you can perform any action with the rating and feedback data, such as sending it to a server or storing it locally.
+  
+  alert('Thank you for your rating and feedback!');
+  
+  // Clear the form inputs
+  document.getElementById('rating').value = '5';
+  document.getElementById('feedback').value = '';
 });
